22a4358df157665fac3c18f7ee5b233828e779a3
Patrick Gebert <patrick.gebert@web.de>
2023-07-18 18:38:20 +0200

    Refactoring.

--- src/commands/checkCommand.ts
+++ src/commands/checkCommand.ts
@@ -1,10 +1,5 @@
 import {BcsClient} from "../bcsClient";
 
-const {prompt} = require('enquirer');
-
-const validateDateInput = (input: string): boolean => /\d{1,2}.\d{1,2}.\d{4}/i.test(input);
-const dateFromString = (value: string): Date => new Date(value.split(".").reverse().join("-"));
-
 export const handleCheckCommand = async () => {
 
     const bcsClient = await BcsClient.getInstance();
