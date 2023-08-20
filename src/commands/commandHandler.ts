9836933c9515369e013912117a1417e255ee0c90
Patrick Gebert <patrick.gebert@web.de>
2023-08-20 21:14:35 +0200

    Allowing escape from prompt.

--- src/commands/commandHandler.ts
+++ src/commands/commandHandler.ts
@@ -10,33 +10,33 @@ import {handleMappingCommand} from "./mappingCommand";
 
 export const handleCommand = async (command: string) => {
 
-    switch (command) {
-        case 'add':
-            await handleAddCommand()
-                .catch(console.error);
-            break;
-        case 'get':
-            await handleGetCommand()
-                .catch(console.error);
-            break;
-        case 'reset':
-            await handleResetCommand()
-                .catch(console.error);
-            break;
-        case 'check':
-            await handleCheckCommand()
-                .catch(console.error);
-            break;
-        case 'mapping':
-            await handleMappingCommand()
-                .catch(console.error);
-            break;
-        case 'quit':
-            BcsClient.getInstance().then((client) => client.close());
-            console.log('Goodbye!')
-            exit(0);
-        default:
-            console.log("Not implemented yet.");
-            break;
+    try {
+        switch (command) {
+            case 'add':
+                await handleAddCommand()
+                break;
+            case 'get':
+                await handleGetCommand()
+                break;
+            case 'reset':
+                await handleResetCommand()
+                break;
+            case 'check':
+                await handleCheckCommand()
+                break;
+            case 'mapping':
+                await handleMappingCommand()
+                break;
+            case 'quit':
+                BcsClient.getInstance().then((client) => client.close());
+                console.log('Goodbye!')
+                exit(0);
+            default:
+                console.log("Not implemented yet.");
+                break;
+        }
+    } catch (e) {
+        console.error(e)
     }
+
 }
